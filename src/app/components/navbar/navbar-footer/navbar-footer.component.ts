28bda08d62dfbd3fcbeced660401b98fa7b811ac
ringtail003 <ringtail003@gmail.com>
2021-06-27 11:57:46 +0900

    クリップボードにURLをコピー

--- src/app/components/navbar/navbar-footer/navbar-footer.component.ts
+++ src/app/components/navbar/navbar-footer/navbar-footer.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from "@angular/core";
+import * as Rx from "rxjs";
+import { delay, tap } from "rxjs/operators";
 
 @Component({
   selector: "app-navbar-footer",
@@ -8,7 +10,19 @@ import { Component, Input, OnInit } from "@angular/core";
 export class NavbarFooterComponent implements OnInit {
   @Input() url!: string;
 
+  copied = false;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  handleClick(): void {
+    Rx.from(navigator.clipboard.writeText(this.url))
+      .pipe(
+        tap({ next: () => (this.copied = true) }),
+        delay(3000),
+        tap({ next: () => (this.copied = false) })
+      )
+      .subscribe();
+  }
 }
