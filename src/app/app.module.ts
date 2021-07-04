48b6ae00aea491d3f78c5830b1e976677f4662f5
ringtail003 <ringtail003@gmail.com>
2021-07-05 08:14:53 +0900

    共有コンポーネントをsharedに移動

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { NgxDropzoneModule } from "ngx-dropzone";
-import { IconLoadingComponent } from "src/app/components/icons/icon-loading/icon-loading.component";
 import { NavbarEffectsMenuComponent } from "src/app/components/navbar/navbar-effects-menu/navbar-effects-menu.component";
 import { NavbarFooterComponent } from "src/app/components/navbar/navbar-footer/navbar-footer.component";
 import { NavbarSettingComponent } from "src/app/components/navbar/navbar-setting/navbar-setting.component";
 import { PreviewComponent } from "src/app/components/preview/preview.component";
+import { IconLoadingComponent } from "src/app/components/shared/icons/icon-loading/icon-loading.component";
 import { AppComponent } from "./app.component";
-import { IconChevronComponent } from "./components/icons/icon-chevron-down/icon-chevron-down.component";
-import { IconChevronLeftComponent } from "./components/icons/icon-chevron-left/icon-chevron-left.component";
-import { IconClipboardComponent } from "./components/icons/icon-clipboard/icon-clipboard.component";
-import { IconCogComponent } from "./components/icons/icon-cog/icon-cog.component";
-import { LogoComponent } from "./components/logo/logo.component";
 import { NavbarButtonComponent } from "./components/navbar/navbar-button/navbar-button.component";
 import { NavbarEffectsComponent } from "./components/navbar/navbar-effects/navbar-effects.component";
 import { NavbarComponent } from "./components/navbar/navbar/navbar.component";
@@ -20,6 +15,12 @@ import { FilenameSettingComponent } from "./components/navbar/settings/filename-
 import { ResizeSettingComponent } from "./components/navbar/settings/resize-setting/resize-setting.component";
 import { RotateSettingComponent } from "./components/navbar/settings/rotate-setting/rotate-setting.component";
 import { ShadowSettingComponent } from "./components/navbar/settings/shadow-setting/shadow-setting.component";
+import { IconChevronComponent } from "./components/shared/icons/icon-chevron-down/icon-chevron-down.component";
+import { IconChevronLeftComponent } from "./components/shared/icons/icon-chevron-left/icon-chevron-left.component";
+import { IconClipboardComponent } from "./components/shared/icons/icon-clipboard/icon-clipboard.component";
+import { IconCogComponent } from "./components/shared/icons/icon-cog/icon-cog.component";
+import { InputNumberComponent } from "./components/shared/input/input-number/input-number.component";
+import { LogoComponent } from "./components/shared/logo/logo.component";
 import { UploaderComponent } from "./components/uploader/uploader.component";
 
 @NgModule({
@@ -45,6 +46,7 @@ import { UploaderComponent } from "./components/uploader/uploader.component";
     ShadowSettingComponent,
     RotateSettingComponent,
     FilenameSettingComponent,
+    InputNumberComponent,
   ],
   imports: [BrowserModule, NgxDropzoneModule],
   providers: [],
