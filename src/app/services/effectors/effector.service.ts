0745c03123d9096b68b5260ed52234f8324464cd
ringtail003 <ringtail003@gmail.com>
2021-07-11 19:18:48 +0900

    effectorの処理を戻す

--- src/app/services/effectors/effector.service.ts
+++ src/app/services/effectors/effector.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import * as Rx from "rxjs";
+import { mergeMap } from "rxjs/operators";
 import { Canvas } from "src/app/models/canvas/canvas";
 import { Effect } from "src/app/models/effect/effect";
 import { plugins } from "src/app/services/effectors/plugins";
@@ -11,7 +12,9 @@ export class EffectorService {
   constructor() {}
 
   effect(canvas: Canvas, effect: Effect): Rx.Observable<Canvas> {
-    return plugins.border(canvas, effect);
+    return plugins
+      .resize(canvas, effect)
+      .pipe(mergeMap((source) => plugins.border(source, effect)));
 
     // return resize(canvas, effect).pipe(
     // mergeMap((source) => plugins.rotate(source, effect)),
