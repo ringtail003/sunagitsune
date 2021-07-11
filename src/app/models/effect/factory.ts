5fa53224c9b34a4bd8421521eadb3c260cdefacc
ringtail003 <ringtail003@gmail.com>
2021-07-11 22:50:22 +0900

    メニューの切り替えがうまくいっていなかった

--- src/app/models/effect/factory.ts
+++ src/app/models/effect/factory.ts
@@ -1,8 +1,10 @@
 import { EffectMetadata } from "src/app/models/effect/effect-metadata";
 import { borderResetMetadata } from "src/app/models/effect/plugin/border-effect";
+import { rotateResetMetadata } from "src/app/models/effect/plugin/rotate-effect";
 
 export const createEffectMetadata = (): EffectMetadata => {
   return {
     ...borderResetMetadata,
+    ...rotateResetMetadata,
   };
 };
