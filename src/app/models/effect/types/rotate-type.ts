36f358d227584e7ef3205ab277216a09100e8bc7
ringtail003 <ringtail003@gmail.com>
2021-07-11 23:00:56 +0900

    左回転いらなかった

--- src/app/models/effect/types/rotate-type.ts
+++ src/app/models/effect/types/rotate-type.ts
@@ -1,10 +1,8 @@
 export const rotateTypeConfig = {
   none: "none",
-  right90: "right 90°",
-  right180: "right 180°",
-  right270: "right 270°",
-  left90: "left 90°",
-  left270: "left 270°",
+  rotate90: "90°",
+  rotate180: "180°",
+  rotate270: "270°",
 };
 
 export type RotateType = keyof typeof rotateTypeConfig;
