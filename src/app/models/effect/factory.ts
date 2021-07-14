fc1a33276de81bc62cc52c97ddf5cf16beebf021
ringtail003 <ringtail003@gmail.com>
2021-07-14 12:44:32 +0900

    resizeのアイコン入れ替え

--- src/app/models/effect/factory.ts
+++ src/app/models/effect/factory.ts
@@ -2,11 +2,13 @@ import { EffectMetadata } from "src/app/models/effect/effect-metadata";
 import { borderResetMetadata } from "src/app/models/effect/plugin/border-effect";
 import { resizeResetMetadata } from "src/app/models/effect/plugin/resize-effect";
 import { rotateResetMetadata } from "src/app/models/effect/plugin/rotate-effect";
+import { shadowResetMetadata } from "src/app/models/effect/plugin/shadow-effect";
 
 export const createEffectMetadata = (): EffectMetadata => {
   return {
     ...borderResetMetadata,
     ...rotateResetMetadata,
     ...resizeResetMetadata,
+    ...shadowResetMetadata,
   };
 };
