8cb24695226342a67c8251b6be8ac5e740412cbd
ringtail003 <ringtail003@gmail.com>
2021-07-18 12:30:35 +0900

    外から受け取った値に副作用を持たせないように修正

--- src/app/components/shared/input/input-color/input-color.component.ts
+++ src/app/components/shared/input/input-color/input-color.component.ts
@@ -1,21 +1,34 @@
-import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+} from "@angular/core";
 
 @Component({
   selector: "app-input-color",
   templateUrl: "./input-color.component.html",
   styleUrls: ["./input-color.component.scss"],
 })
-export class InputColorComponent implements OnInit {
+export class InputColorComponent implements OnInit, OnChanges {
   @Input() value: string | null = null;
   @Output() changeValue = new EventEmitter<string | null>();
   @Input() error: string | null = null;
+
+  color!: string | null;
 
   constructor() {}
 
   ngOnInit(): void {}
 
+  ngOnChanges(): void {
+    this.color = this.value;
+  }
+
   handleChange(): void {
-    this.changeValue.emit(this.value);
+    this.changeValue.emit(this.color);
   }
 
   handleClear(): void {
