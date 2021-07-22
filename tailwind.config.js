ba0570d90ceebacde202e6713638b75d1db33b71
ringtail003 <ringtail003@gmail.com>
2021-07-22 11:33:53 +0900

    footerのシャドウを上向きに

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
   purge: ['./src/**/*.{html,ts}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'reverse-lg': '0 -10px 15px -3px rgba(0, 0, 0, 0.1), 0 -4px 6px -2px rgba(0, 0, 0, 0.05)',
+      }
+    },
   },
   variants: {
     extend: {},
