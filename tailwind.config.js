0cdf648343a381e9336ca589090bff6fbb4d8dc0
ringtail003 <ringtail003@gmail.com>
2021-08-08 08:46:38 +0900

    エフェクトによりページ分割が伸縮しないように

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,10 @@ module.exports = {
     extend: {
       boxShadow: {
         'reverse-lg': '0 -10px 15px -3px rgba(0, 0, 0, 0.1), 0 -4px 6px -2px rgba(0, 0, 0, 0.05)',
-      }
+      },
+      width: {
+        'preview': '450px'
+      },
     },
   },
   variants: {
